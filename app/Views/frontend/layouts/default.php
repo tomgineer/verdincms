b9406d56f910e35a63d69b9dcc6ea25fb4b2f49d
Tom Papatolis <49147550+tompapatolis@users.noreply.github.com>
2025-12-26 09:46:34 +0100

    Added font loader

--- app/Views/frontend/layouts/default.php
+++ app/Views/frontend/layouts/default.php
@@ -1,55 +1,60 @@
 <!DOCTYPE html>
-<html lang="<?=setting('site.language')?>" data-theme="<?=setting('theme.name')?>">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <?= body_class() === 'site-index' ? csrf_meta() : '' ?>
-        <meta name="base-url" content="<?=base_url();?>">
-        <meta name="color-scheme" content="light dark">
+<html lang="<?= setting('site.language') ?>" data-theme="<?= setting('theme.name') ?>">
 
-        <?php if (!empty($post)): ?>
-            <title><?= htmlspecialchars($post['title']) ?> | <?= setting('meta.siteName') ?></title>
-            <meta name="description" content="<?= htmlspecialchars($post['subtitle']) ?>"/>
-            <?= $this->include('frontend/partials/meta_post') ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?= body_class() === 'site-index' ? csrf_meta() : '' ?>
+    <meta name="base-url" content="<?= base_url(); ?>">
+    <meta name="color-scheme" content="light dark">
 
-        <?php elseif (!empty($page)): ?>
-            <title><?= htmlspecialchars($page['title']) ?> | <?= setting('meta.siteName') ?></title>
-            <meta name="description" content="<?= htmlspecialchars($page['subtitle']) ?>"/>
-            <?= $this->include('frontend/partials/meta_page') ?>
+    <?php if (!empty($post)): ?>
+        <title><?= htmlspecialchars($post['title']) ?> | <?= setting('meta.siteName') ?></title>
+        <meta name="description" content="<?= htmlspecialchars($post['subtitle']) ?>" />
+        <?= $this->include('frontend/partials/meta_post') ?>
 
-        <?php else: ?>
-            <title><?= (!empty($site_title) ? $site_title . ' | ' . setting('meta.siteName') : setting('meta.siteTitle')) ?></title>
-            <meta name="description" content="<?= setting('meta.siteDescription') ?>"/>
-        <?php endif; ?>
+    <?php elseif (!empty($page)): ?>
+        <title><?= htmlspecialchars($page['title']) ?> | <?= setting('meta.siteName') ?></title>
+        <meta name="description" content="<?= htmlspecialchars($page['subtitle']) ?>" />
+        <?= $this->include('frontend/partials/meta_page') ?>
 
-        <link rel="stylesheet" href="<?=path_css()?>tailwind.css?v=<?=setting('system.version')?>">
-        <link rel="stylesheet" href="<?=path_assets()?>fonts/fonts.css">
+    <?php else: ?>
+        <title><?= (!empty($site_title) ? $site_title . ' | ' . setting('meta.siteName') : setting('meta.siteTitle')) ?></title>
+        <meta name="description" content="<?= setting('meta.siteDescription') ?>" />
+    <?php endif; ?>
 
-        <?php if ( !empty($highlight) ): ?>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/styles/github-dark.min.css">
-            <script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/highlight.min.js"></script>
-        <?php endif;?>
+    <link rel="stylesheet" href="<?= path_css() ?>tailwind.css?v=<?= setting('system.version') ?>">
 
-        <script src="<?=path_js()?>app-dist.js?v=<?=setting('system.version')?>" defer></script>
-        <?= $this->include('frontend/partials/favicon') ?>
-        <?= $this->renderSection('head') ?>
+    <?php foreach (setting('theme.extraFonts') ?: ['default'] as $font): ?>
+        <link rel="stylesheet" href="<?= path_assets() ?>fonts/<?= esc($font) ?>/fonts.css">
+    <?php endforeach; ?>
 
-    </head>
+    <?php if (!empty($highlight)): ?>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/styles/github-dark.min.css">
+        <script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/highlight.min.js"></script>
+    <?php endif; ?>
 
-    <body class="min-h-dvh flex flex-col <?=body_class()?>" id="<?=body_class()?>">
+    <script src="<?= path_js() ?>app-dist.js?v=<?= setting('system.version') ?>" defer></script>
+    <?= $this->include('frontend/partials/favicon') ?>
+    <?= $this->renderSection('head') ?>
 
-        <header>
-            <?=view_cell('FrontendCell::nav')?>
-            <?= $this->renderSection('header') ?>
-        </header>
+</head>
 
-        <main class="flex-1">
-            <?= $this->renderSection('main') ?>
-        </main>
+<body class="min-h-dvh flex flex-col <?= body_class() ?>" id="<?= body_class() ?>">
 
-        <footer class="bg-base-200" data-theme="dark">
-            <?=view_cell('FrontendCell::footer')?>
-        </footer>
+    <header>
+        <?= view_cell('FrontendCell::nav') ?>
+        <?= $this->renderSection('header') ?>
+    </header>
 
-    </body>
+    <main class="flex-1">
+        <?= $this->renderSection('main') ?>
+    </main>
+
+    <footer class="bg-base-200" data-theme="dark">
+        <?= view_cell('FrontendCell::footer') ?>
+    </footer>
+
+</body>
+
 </html>
