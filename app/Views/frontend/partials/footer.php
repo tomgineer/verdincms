0440620119d5e84372faa6679739e74df924ccf8
Tom Papatolis <49147550+tompapatolis@users.noreply.github.com>
2026-01-12 14:44:13 +0100

    Small Update

--- app/Views/frontend/partials/footer.php
+++ app/Views/frontend/partials/footer.php
@@ -20,7 +20,7 @@
                 <h2 class="menu-title text-lg"><?=lang('App.mini_stas')?></h2>
                 <ul>
                     <li><div><?=lang('App.public_posts')?>: <span class="text-accent"><?= esc($public_posts) ?></span></div></li>
-                    <li><div><?=lang('App.subscribers')?>: <span class="text-accent"><?= esc($total_subscribers) ?></span></div></li>
+                    <li class="hidden"><div><?=lang('App.subscribers')?>: <span class="text-accent"><?= esc($total_subscribers) ?></span></div></li>
                     <li><div><?=lang('App.cms_version')?>: <span class="text-accent"><?= VERDINCMS_VERSION ?></span></div></li>
                     <li><div>Rendered in: <span class="text-accent">{elapsed_time} sec</span></div></li>
                 </ul>
