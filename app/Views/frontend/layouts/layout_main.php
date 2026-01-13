db4c45889325790657519e357f9ac91225e5f0de
Tom Papatolis <49147550+tompapatolis@users.noreply.github.com>
2026-01-13 11:25:05 +0100

    New main layout finished

--- app/Views/frontend/layouts/layout_main.php
+++ app/Views/frontend/layouts/layout_main.php
@@ -10,23 +10,67 @@
     <meta name="description" content="<?= setting('meta.siteDescription') ?>" />
 
     <?= $this->include('frontend/layouts/scripts') ?>
-    <?= $this->include('frontend/layouts/stylesheets') ?>
+    <?= $this->include('frontend/layouts/styles') ?>
     <?= $this->renderSection('head') ?>
 </head>
 
 <body class="min-h-screen flex flex-col <?= body_class() ?>">
 
-    <header>
+    <header class="border-2 border-blue-800" data-region="header">
         <?= view_cell('FrontendCell::nav') ?>
         <?= $this->renderSection('header') ?>
     </header>
 
-    <main class="flex-1">
-        <?= $this->renderSection('main') ?>
+    <main class="flex flex-col gap-8 lg:gap-12 2xl:gap-16">
+        <div class="border-2 border-blue-800" data-region="hero">
+            <?= $this->renderSection('hero') ?>
+        </div>
+
+        <div class="px-4 xl:container xl:mx-auto border-2 border-blue-800" data-region="top">
+            <?= $this->renderSection('top') ?>
+        </div>
+
+        <div class="px-2 lg:px-4 xl:container xl:mx-auto border-2 border-blue-800">
+            <div class="flex flex-col lg:flex-row gap-8 lg:gap-12 border-2 border-blue-800">
+                <section class="flex-1 border-2 border-blue-800" aria-label="Main top content">
+                    <?= $this->renderSection('main_top') ?>
+                </section>
+
+                <aside class="w-full lg:w-[30%] hidden lg:block border-2 border-blue-800" aria-label="Sidebar top">
+                    <?= $this->renderSection('sidebar_top') ?>
+                </aside>
+            </div>
+        </div>
+
+        <div class="border-2 border-blue-800" data-region="middle">
+            <?= $this->renderSection('middle') ?>
+        </div>
+
+        <div class="px-2 lg:px-4 xl:container xl:mx-auto border-2 border-blue-800">
+            <div class="flex flex-col lg:flex-row gap-8 lg:gap-12 border-2 border-blue-800">
+                <section class="flex-1 border-2 border-blue-800" aria-label="Main bottom content">
+                    <?= $this->renderSection('main_bottom') ?>
+                </section>
+
+                <aside class="w-full lg:w-[30%] border-2 border-blue-800" aria-label="Sidebar bottom">
+                    <?= $this->renderSection('sidebar_bottom') ?>
+                </aside>
+            </div>
+        </div>
+
+        <div class="px-4 xl:container xl:mx-auto border-2 border-blue-800" data-region="bottom">
+            <?= $this->renderSection('bottom') ?>
+        </div>
     </main>
 
-    <footer data-theme="<?=setting('theme.darkBlocks')?>">
-        <?= view_cell('FrontendCell::footer') ?>
+    <footer class="mt-auto flex flex-col border-2 border-blue-800">
+        <div class="border-2 border-blue-800" data-region="call-to-action">
+            <?= $this->renderSection('call_to_action') ?>
+        </div>
+
+        <div class="border-2 border-blue-800" data-region="footer">
+            <?= $this->renderSection('footer') ?>
+        </div>
     </footer>
 
 </body>
--- app/Views/frontend/pages/test.php
+++ app/Views/frontend/pages/test.php
@@ -1,6 +1,41 @@
 <?= $this->extend('frontend/layouts/layout_main') ?>
 
-<?= $this->section('main') ?>
-    <h1>Test</h1>
+<?= $this->section('hero') ?>
+    <span>hero</span>
+<?= $this->endSection() ?>
 
+<?= $this->section('top') ?>
+    <span>Top</span>
+<?= $this->endSection() ?>
+
+<?= $this->section('main_top') ?>
+    <span>Main Top</span>
+<?= $this->endSection() ?>
+
+<?= $this->section('sidebar_top') ?>
+    <span>Sidebar Top</span>
+<?= $this->endSection() ?>
+
+<?= $this->section('middle') ?>
+    <span>Middle</span>
+<?= $this->endSection() ?>
+
+<?= $this->section('main_bottom') ?>
+    <span>Main Bottom</span>
+<?= $this->endSection() ?>
+
+<?= $this->section('sidebar_bottom') ?>
+    <span>Sidebar Bottom</span>
+<?= $this->endSection() ?>
+
+<?= $this->section('bottom') ?>
+    <span>Bottom</span>
+<?= $this->endSection() ?>
+
+<?= $this->section('call_to_action') ?>
+    <span>Call to Action</span>
+<?= $this->endSection() ?>
+
+<?= $this->section('footer') ?>
+    <?= view_cell('FrontendCell::footer') ?>
 <?= $this->endSection() ?>
